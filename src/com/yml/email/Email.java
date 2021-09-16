b885fdbc5200abd8536b4222688d4dfd6fc4808d
Ankitha Kamath <ankithakamath@196AMBBLR-Ankitha-Kamath.local>
2021-09-17 01:52:55 +0530

    the Use Case 4 of email is added

--- src/com/yml/email/Email.java
+++ src/com/yml/email/Email.java
@@ -5,8 +5,9 @@ import java.util.regex.Pattern;
 public class Email {
 	public static void main(String[] args) {
 
-		String email = ".co";
-		String pattern = "\\.[a-z]{2,4}";  // regular expression for validating third mandatory part
+		String email = "abc.xyz@bridgelabz.com";
+		String pattern = "^[a-zA-z0-9]+?(.)[a-zA-Z0-9+_.-]*@[a-zA-Z]+\\.[a-zA-z]{2,3}$"; 
 		System.out.println(Pattern.matches(pattern, email));
+
 	}
 }
