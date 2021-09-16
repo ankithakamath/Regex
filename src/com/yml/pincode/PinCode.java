ef8a2b5da14c7f11da51ef32fc6cb552b902c0ac
Ankitha Kamath <ankithakamath@196AMBBLR-Ankitha-Kamath.local>
2021-09-17 01:39:48 +0530

    Use Case 3 of pincode added

--- src/com/yml/pincode/PinCode.java
+++ src/com/yml/pincode/PinCode.java
@@ -6,8 +6,8 @@ public class PinCode {
 
 	public static void main(String[] args) {
 
-		String pincode = "A400088"; // string to be validated
-		String pattern = "[1-9]{1}[0-9]{5}"; // regular expression
+		String pincode = "400088A"; // string to be validated
+		String pattern = "[1-9]{1}[0-9]{5}$"; // regular expression
 		System.out.println(Pattern.matches(pattern, pincode));
 	}
 
